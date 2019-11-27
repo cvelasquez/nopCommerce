be30103fda0b3db0197a7ed76ad42997b4287217
christian.velasquez <christian.velasquez@laytaruma.com.pe>
2019-11-27 16:45:57 -0500

    Update SwipeSliderPro and MinimalWhite theme

---
+++ src/Plugins/Nop.Plugin.Widgets.SwipeSliderPro/SwipeTypes/CoverflowEffect3D.cs
@@ -0,0 +1,19 @@
+﻿using Nop.Core.Configuration;
+
+namespace Nop.Plugin.Widgets.SwipeSliderPro.SwipeTypes
+{
+    public class CoverflowEffect3D : ISettings
+    {
+        public bool loop { get; set; }
+        public bool freeMode { get; set; }
+        public bool autoPlay { get; set; }
+        public int autoPlayDelay { get; set; }
+        public bool autoPlayDisableOnInteraction { get; set; }
+    }
+
+    public enum AspectRelation
+    {
+        Books,
+        Music
+    }
+}
